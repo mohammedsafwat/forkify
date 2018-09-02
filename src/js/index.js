3f95a65739d06d7dfc73fadef3365230feac3624
Mohammed Safwat <mail.msafwat@gmail.com>
2018-09-03 00:34:13 +0200

    Add 'index.js' controller file.

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements, elementStrings, renderLoader, clearLoader } from './views/base'
+
+/**
+ * Global State of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+    // 1) Get the query from the view
+    const query = searchView.getInput();
+
+    // 2) Create a new search object and add it to state
+    if (query) {
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchResultsParent);
+
+        // 4) Search for recipes
+        // Every async function returns a promise
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResultsPages.addEventListener('click', e => {
+    const btn = e.target.closest(`.${elementStrings.resultsPageButton}`);
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.clearResultsButtons();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+    }
+});
