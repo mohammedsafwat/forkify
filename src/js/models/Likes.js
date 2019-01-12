fa5732fc243de9a53c682d16f1ce6efac6b8a400
Mohammed Safwat <mail.msafwat@gmail.com>
2019-01-12 10:19:07 +0100

    Add a new model class for the 'Likes' list.

---
+++ src/js/models/Likes.js
@@ -0,0 +1,25 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, author, image) {
+        const like = { id, title, author, image };
+        this.likes.push(like);
+        return like;
+    } 
+
+    deleteLike(id) {
+        const index = this.likes.findIndex(el => el.id === id);
+        this.likes.splice(index, 1);
+    }
+
+    isLiked(id) {
+        console.log(this.likes.findIndex(el => el.id === id) !== -1);
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
+}
